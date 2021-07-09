901b29b307e351ab48098a16a94b5fababf0ab21
Michael Lingelbach <m.j.lbach@gmail.com>
2021-07-09 14:31:00 -0700

    chore: cleanup packer

--- init.lua
+++ init.lua
@@ -1,10 +1,8 @@
 -- Install packer
-local execute = vim.api.nvim_command
-
 local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
 
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
+  vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
 end
 
 vim.api.nvim_exec(
