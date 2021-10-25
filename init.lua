9bd907b7f5aacd6363fcf206b9240ffb7b9db3e7
Michael Lingelbach <m.j.lbach@gmail.com>
2021-10-25 14:47:59 -0700

    chore: remove redundant require

--- init.lua
+++ init.lua
@@ -286,7 +286,7 @@ local cmp = require 'cmp'
 cmp.setup {
   snippet = {
     expand = function(args)
-      require('luasnip').lsp_expand(args.body)
+      luasnip.lsp_expand(args.body)
     end,
   },
   mapping = {
