7dd35e78711eace209ba82d16b2b13674a16048d
Michael Lingelbach <m.j.lbach@gmail.com>
2021-11-30 17:37:38 -0500

    fix: remove deprecated indent-blankline option

--- init.lua
+++ init.lua
@@ -94,7 +94,6 @@ vim.cmd [[
 vim.g.indent_blankline_char = '┊'
 vim.g.indent_blankline_filetype_exclude = { 'help', 'packer' }
 vim.g.indent_blankline_buftype_exclude = { 'terminal', 'nofile' }
-vim.g.indent_blankline_char_highlight = 'LineNr'
 vim.g.indent_blankline_show_trailing_blankline_indent = false
 
 -- Gitsigns
