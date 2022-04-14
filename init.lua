f3286d94393df6944d39cdbde63c4744a100a034
Michael Lingelbach <m.j.lbach@gmail.com>
2022-04-14 08:09:58 -0700

    fix: use cmp.mapping.preset.insert

--- init.lua
+++ init.lua
@@ -278,13 +278,10 @@ cmp.setup {
       luasnip.lsp_expand(args.body)
     end,
   },
-  mapping = {
-    ['<C-p>'] = cmp.mapping.select_prev_item(),
-    ['<C-n>'] = cmp.mapping.select_next_item(),
+  mapping = cmp.mapping.preset.insert({
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.close(),
     ['<CR>'] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
@@ -307,7 +304,7 @@ cmp.setup {
         fallback()
       end
     end,
-  },
+  }),
   sources = {
     { name = 'nvim_lsp' },
     { name = 'luasnip' },
