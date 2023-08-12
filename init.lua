a98835ef0edc99883a8f25a6898d0c8496a1ccba
Brian Salkas <brian.salkas@gmail.com>
2023-08-12 12:06:57 -0500

    fix: vim local variable declaration (#67)

--- init.lua
+++ init.lua
@@ -1,5 +1,4 @@
 -- Install package manager
-local vim
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system { 'git', 'clone', '--filter=blob:none', 'https://github.com/folke/lazy.nvim.git', '--branch=stable', lazypath }
